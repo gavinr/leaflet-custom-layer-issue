9908de4eed46a098d682d0f6106c620266e8b1c2
Gavin Rehkemper <gavin@gavinr.com>
2021-02-13 10:29:00 -0600

    custom layer test case

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,17 +1,34 @@
 <script>
-  import { Map as LeafletMap, Icon, Marker } from "leaflet";
+  import { Map as LeafletMap, Layer, Icon, Marker } from "leaflet";
   import "leaflet/dist/leaflet.css";
+
+  const CustomLayer = Layer.extend({
+    onAdd: function (map) {
+      console.log("CUSTOM LAYER ONADD");
+      const size = map.getSize();
+      console.log("size:", size);
+    },
+  });
+
+  const customLayer = (key, options) => {
+    return new CustomLayer(key, options);
+  };
 
   const map = (domNode) => {
     const map = new LeafletMap(domNode);
     map.setView([53.35014, -6.266155], 8);
-    
 
-    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
+    var OpenStreetMap_Mapnik = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }
+    );
     map.addLayer(OpenStreetMap_Mapnik);
+
+    customLayer().addTo(map);
   };
 </script>
 
