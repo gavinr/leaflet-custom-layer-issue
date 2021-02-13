4e3a6b6f11780b00483223c916e5693a97829925
Gavin Rehkemper <gavin@gavinr.com>
2021-02-13 10:22:34 -0600

    leaflet map working in svelte - basic setup

---
+++ src/Map.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Map as LeafletMap, Icon, Marker } from "leaflet";
+  import "leaflet/dist/leaflet.css";
+
+  const map = (domNode) => {
+    const map = new LeafletMap(domNode);
+    map.setView([53.35014, -6.266155], 8);
+    
+
+    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    map.addLayer(OpenStreetMap_Mapnik);
+  };
+</script>
+
+<div use:map />
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+  }
+</style>
